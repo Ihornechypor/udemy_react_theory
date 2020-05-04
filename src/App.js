72ec86ac05c2cfe4a9a31ee2918e8ba092645ce2
chip_a <chip_a@ukr.net>
2020-05-04 12:23:11 +0200

    lesson 14 method onclick

--- src/App.js
+++ src/App.js
@@ -13,6 +13,10 @@ class App extends Component {
     pageTitle: 'Hello react state'
   };
 
+  changeTitleHandler = () => {
+    console.log('clicked');
+  };
+
   render(){
     const  divStyle = {
       textAlign: 'center'
@@ -20,12 +24,14 @@ class App extends Component {
 
     const cars = this.state.cars;
     
-
     return (
       <div style={divStyle}>
         <h1>
           {this.state.pageTitle}
         </h1>
+        <button onClick={this.changeTitleHandler}> 
+          Change title
+        </button>
         <Car name={cars[0].name} year={cars[0].year} />
         <Car name={cars[1].name} year={cars[1].year} />
         <Car name={cars[2].name} year={cars[2].year} />

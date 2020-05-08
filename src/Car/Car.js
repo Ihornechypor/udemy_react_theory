af971e16d813b0393619192770734852684a21a7
chip_a <chip_a@ukr.net>
2020-05-08 11:48:58 +0200

    lesson 23 arrayed styles

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import './car.css';
 
-export default props =>(
+export default props => {
+    const inputClass = ['input'];
+
+    if(props.name){
+        inputClass.push('green');
+    } else {
+        inputClass.push('red')
+    }
+
+    if(props.name.length > 4){
+        inputClass.push('bold')
+    }
+
+    return(
         <div className='Car'>
             <h3>
                 Car name: {props.name}
@@ -10,11 +23,14 @@ export default props =>(
                 <strong>
                    Year {props.year}
                 </strong>
- 
             </p>
-            <input type="text" onChange={props.onChangeName} value={props.name}/>
+            <input type="text" 
+                   onChange={props.onChangeName} 
+                   value={props.name}
+                   className={inputClass.join(' ')}
+            />
             <button onClick={props.onDelete}>
                 Delete
             </button>  
         </div>
-);
+)};
